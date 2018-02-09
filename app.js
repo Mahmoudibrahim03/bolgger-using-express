11108824a085dedc02db755dcb35cb871c356d35
Mahmoud Ibrahim <xmanali92@gmail.com>
2018-02-09 02:48:22 +0200

    file structure

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ var fs = require("fs");
 var express = require("express");
 var ejs = require("ejs");
 var bodyParser = require("body-parser");
-var cookieParser = require("cookie-parser");
 var app = express();
 
 //Static files and engine ⛔⛔⛔
 app.set("view engine", "ejs");
 app.use("/assets", express.static("assets"));
-//body parser initialization
 var jsonParser = bodyParser.json();
 var urlencodedParser = bodyParser.urlencoded({
-  extended: false
+    extended: false
 });
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-
+// Dynamic controller GET,POST 📧📧📧
+// Home page .
+app.get("/", (req, res) => {
+    res.render("index.ejs",{
+        pageTitle:"Home"
+    })
+})
 
 
 
@@ -24,4 +30,4 @@ app.use(bodyParser.urlencoded());
 
 app.listen(2020, () => {
     console.log("server work in port 2020");
-  });
+});
